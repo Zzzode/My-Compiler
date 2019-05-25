b826f42cbaef64ff182cec31876429d446f24d66
zxhcho <1441904450@qq.com>
2019-05-25 09:16:15 +0800

    finish lexical analysis

--- lexical/lexical.h
+++ lexical/lexical.h
@@ -2,8 +2,8 @@
 #define _LEXICAL_H_
 //headfile for lexical
 
-typedef enum{
-    OPERATOR, RESERVED_WORD,
+enum Token_type{
+    OPERATOR, RESERVED,
     //system type
     SYS_CON, SYS_FUN, SYS_PROC, SYS_TYPE,
     //identify an id
@@ -12,14 +12,14 @@ typedef enum{
     TYPE_INT, TYPE_REAL, TYPE_CHAR, TYPE_BOOL, TYPE_STR,
     //literal constant
     CONST_INT, CONST_REAL, CONST_CHAR, CONST_BOOL, CONST_STR
-} Token_type;
+};
 
-typedef enum{
+enum Token_name{
     //operators
-    LP = 18, RP, LB, RB, DOT, COMMA, COLON, MUL, DIV, DIV_R, UNEQUAL, NOT,
-    PLUS, MINUS, GE, GT, LE, LT, EQUAL, ASSIGN, MOD, DOTDOT, SEMI,
+    LP = 17, RP, LB, RB, DOT, COMMA, COLON, MUL, DIV, DIV_R, UNEQUAL, NOT,
+    PLUS, MINUS, GE, GT, LE, LT, EQUAL, NE, ASSIGN, MOD, DOTDOT, SEMI,
     // reserve words
-    AND, ARRAY, BEGIN, CASE, CONST, DO, DOWNTO, ELSE,
+    AND, ARRAY, MY_BEGIN, CASE, CONST, DO, DOWNTO, ELSE,
     END, FOR, FUNCT, GOTO, IF, IN, OF, OR, PACKED, 
     PROC, PROGRAM, RECORD, REPEAT, SET, THEN, TO, 
     TYPE, UNTIL, VAR, WHILE, WITH,
@@ -30,19 +30,21 @@ typedef enum{
     // SYS_PROC
     WRITE, WRITELN, READ,
     // SYS_TYPE for declaration
-    BOOLEAN, CHAR, INTEGER, REAL, STR
-} Token_name; 
+    BOOL, CHAR, INT, REAL, STR
+} ; 
 
 typedef struct token_attri{
     int token_type; // operator or reserved_word or...
-    int token_name; 
+    int token_index; 
     int data_type; // real, int, bool or...
+    char token_string[32];  //max-length of an id or string 
     union{
-        char token_string[32];  //max-length of an id or string 
         int int_num;    //value for int-type
         char ch;    //value for char-type
         double real_num;    //value for real_type
     }token_val;
 } *Token; // attribute of a token 
+
+void printtoken(Token yylval);
 
 #endif // !_LEXI_H_
